cab465908c2251fef3a99a7c9853db7eb312432e
unknown <revanthmech99@gmail.com>
2026-05-09 19:58:47 +0530

    small update

--- tests/global.setup.ts
+++ tests/global.setup.ts
@@ -2,8 +2,8 @@ import {expect, test} from '../fixture/pom-fixture'
 
 
 test("Global setup for sauce demo application",async({page, loginPage,productsPage})=>{
-    loginPage.openSauceDemo()
-    loginPage.loginSauceDemo(process.env.USER_NAME!,process.env.PASSWORD!)
+    await loginPage.openSauceDemo()
+    await loginPage.loginSauceDemo(process.env.USER_NAME!,process.env.PASSWORD!)
     await page.waitForURL(process.env.BASE_URL!+"inventory.html")
     expect(await productsPage.productPageTitle).toHaveText("Products")
     await page.context().storageState({
